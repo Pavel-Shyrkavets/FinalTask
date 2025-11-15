8fccf50cbfef1e08eb7e78dc55407d4ed545c976
Pavel Shyrkavets <pshyrkavets@gmail.com>
2025-11-15 20:56:48 +0100

    edited the main branch

---
+++ Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace FinalTask
+{
+    public static class Constants
+    {
+        public const string PASSWORD = "secret_sauce";
+        public const string FIRST_USERNAME = "standard_user";
+        public const string SECOND_USERNAME = "problem_user";
+        public const string THIRD_USERNAME = "performance_glitch_user";
+        public const string FOURTH_USERNAME = "error_user";
+        public const string FIFTH_USERNAME = "visual_user";
+    }
+}
--- Tests/FinalTaskTests.cs
+++ Tests/FinalTaskTests.cs
@@ -3,97 +3,64 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 
+[assembly: Parallelize]
 namespace FinalTask.Tests
-{
+{   
     [TestClass]
     public sealed class IndexPageTests
     {
         private readonly IWebDriver chromeDriver = new ChromeDriver();
         private readonly IWebDriver firefoxDriver = new FirefoxDriver();
-        private readonly string credentials = "Automation";
-        private readonly string errorMessage = "Username is required";
-        private readonly string errorPasswordMessage = "Password is required";
+        private readonly string errorMessage = "Username and password do not match any user in this service";
         private readonly string title = "Swag Labs";
+        private readonly string credentials = Config.GetConfigValue("credentials");
 
+        [TestCleanup]
         [TestMethod]
         public void Login_WithoutCredentials_ErrorMessageAppears()
         {
-            try
-            {
-                var indexPage = new IndexPage(chromeDriver);
+            var indexPage = new IndexPage(chromeDriver);
 
-                indexPage.Open().InputCredentials(credentials, credentials);
-                indexPage.ClearCredentials();
-                indexPage.ClickLoginButton();
+            indexPage.Open().InputCredentials(credentials, credentials);
+            indexPage.ClearCredentials();
+            indexPage.ClickLoginButton();
 
-                var fullErrorMessage = indexPage.GetFullErrorMessage();
+            var fullErrorMessage = indexPage.GetFullErrorMessage();
 
-                Assert.Contains(fullErrorMessage, errorMessage);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                chromeDriver.Quit();
-            }
+            Assert.Contains(errorMessage, fullErrorMessage);      
         }
 
         [TestMethod]
         public void Login_WithoutPassword_ErrorMessageAppears()
         {
-            try
-            {
-                var indexPage = new IndexPage(chromeDriver);
+            var indexPage = new IndexPage(chromeDriver);
 
-                indexPage.Open().InputCredentials(credentials, credentials);
-                indexPage.ClearPassword();
-                indexPage.ClickLoginButton();
+            indexPage.Open().InputCredentials(credentials, credentials);
+            indexPage.ClearPassword();
+            indexPage.ClickLoginButton();
 
-                var fullPasswordErrorMessage = indexPage.GetFullErrorMessage();
+            var fullErrorMessage = indexPage.GetFullErrorMessage();
 
-                Assert.Contains(fullPasswordErrorMessage, errorPasswordMessage);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                chromeDriver.Quit();
-            }
+            Assert.Contains(errorMessage, fullErrorMessage); 
         }
 
-        [DataRow("standard_user", "secret_sauce")]
-        [DataRow("locked_out_user", "secret_sauce")]
-        [DataRow("problem_user", "secret_sauce")]
-        [DataRow("performance_glitch_user", "secret_sauce")]
-        [DataRow("error_user", "secret_sauce")]
-        [DataRow("visual_user", "secret_sauce")]
-        [TestMethodAttribute]
+        [DataRow(Constants.FIRST_USERNAME, Constants.PASSWORD)]
+        [DataRow(Constants.SECOND_USERNAME, Constants.PASSWORD)]
+        [DataRow(Constants.THIRD_USERNAME, Constants.PASSWORD)]
+        [DataRow(Constants.FOURTH_USERNAME, Constants.PASSWORD)]
+        [DataRow(Constants.FIFTH_USERNAME, Constants.PASSWORD)]
+        [TestMethod]
         public void Login_WithValidCredentials_ErrorMessageAppears(string username, string password)
         {
-            try
-            {
-                var indexPage = new IndexPage(firefoxDriver);
+            var indexPage = new IndexPage(firefoxDriver);
 
-                indexPage.Open().InputCredentials(username, password);
-                indexPage.ClickLoginButton();
+            indexPage.Open().InputCredentials(username, password);
+            indexPage.ClickLoginButton();
 
-                var inventoryPage = new InventoryPage(firefoxDriver);
-                var titleInDashboard = inventoryPage.GetDashboardTitle();
+            var inventoryPage = new InventoryPage(firefoxDriver);
+            var titleInDashboard = inventoryPage.GetDashboardTitle();
 
-                Assert.AreEqual(titleInDashboard, title);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                firefoxDriver.Quit();
-            }
+            Assert.AreEqual(title, titleInDashboard);
         }
     }
 }
